2958a69a3d34dfd6848dcaa3b947d14bb55cc7b6
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-17 09:19:33 +0300

    correcting function

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -4,7 +4,7 @@ export function useForm(steps) {
   const [currentStep, setCurrentStep] = useState(0);
 
   function changeStep(i, e) {
-    e.preventDefault();
+    if (e) e.preventDefault();
 
     if (i < 0 || i >= steps.length) return;
 
