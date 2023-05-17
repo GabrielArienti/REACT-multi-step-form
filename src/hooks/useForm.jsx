be02d56a12a66d1bb1dfe31d58474fafd5594e32
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-17 09:12:37 +0300

    function to multi-step

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 export function useForm(steps) {
   const [currentStep, setCurrentStep] = useState(0);
 
+  function changeStep(i, e) {
+    e.preventDefault();
+
+    if (i < 0 || i >= steps.length) return;
+
+    setCurrentStep(i);
+  }
+
   return {
     currentStep,
     currentComponent: steps[currentStep],
