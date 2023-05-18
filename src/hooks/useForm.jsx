374d04e2b39767b2ea0ea610292e457b8919c3eb
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-18 14:16:43 +0300

    useForm

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export function useForm(steps) {
-  const [currentStep, setCurrentStep] = useState(2);
+  const [currentStep, setCurrentStep] = useState(0);
 
   function changeStep(i, e) {
     if (e) e.preventDefault();
