982c0f385aff4f69bf17fd58a20d65423c5c7ff2
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-19 08:15:53 +0300

    stepper

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -8,15 +8,15 @@ import { FiSend } from "react-icons/fi";
 const Steps = ({ currentStep }) => {
   return (
     <div className="steps">
-      <div className="step">
+      <div className="step active">
         <AiOutlineUser />
         <p>Identificação</p>
       </div>
-      <div className="step">
+      <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
         <AiOutlineStar />
         <p>Avaliação</p>
       </div>
-      <div className="step">
+      <div className={`step ${currentStep >= 2 ? "active" : ""} `}>
         <FiSend />
         <p>Envio</p>
       </div>
