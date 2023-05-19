cede20ba218463c1e86272ca02abc15c45cd89b3
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-19 07:27:39 +0300

    first step function btn

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -16,5 +16,6 @@ export function useForm(steps) {
     currentComponent: steps[currentStep],
     changeStep,
     isLastStep: currentStep + 1 === steps.length ? false : true,
+    isFirstStep: currentStep === 0 ? true : false,
   };
 }
