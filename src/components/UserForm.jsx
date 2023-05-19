be36efb2f119ecf29cf31a1963e68306e71026f3
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-19 07:36:37 +0300

    form userForm

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,9 +2,31 @@ import React from "react";
 
 const UserForm = () => {
   return (
-    <div>
-      <h2>User Form</h2>
-    </div>
+    <>
+      {/* Name */}
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite o seu nome"
+          required
+        />
+      </div>
+
+      {/* Email */}
+      <div className="form-control">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu melhor email"
+          required
+        />
+      </div>
+    </>
   );
 };
 
