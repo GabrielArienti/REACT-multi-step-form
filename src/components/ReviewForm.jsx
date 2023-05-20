f2fa1a84c1abeec88c95fce28d48c1a01ac992ca
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-20 10:44:01 +0300

    review form component

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,56 @@
 import React from "react";
 
+// Components
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
 const ReviewForm = () => {
   return (
-    <div>
-      <h2>Review Form</h2>
+    <div className="revie-form">
+      <div className="form-control score-container">
+        {/* Unastified */}
+        <label className="radio-container">
+          <input type="radio" value="unsatisfied" name="review" required />
+          <BsFillEmojiFrownFill />
+          <p>Instatisfeitos</p>
+        </label>
+
+        {/* Neutral */}
+        <label className="radio-container">
+          <input type="radio" value="neutral" name="review" required />
+          <BsFillEmojiNeutralFill />
+          <p>Neutro</p>
+        </label>
+
+        {/* Satisfied */}
+        <label className="radio-container">
+          <input type="radio" value="satisfied" name="review" required />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+
+        {/* Very Satisfied */}
+        <label className="radio-container">
+          <input type="radio" value="very_satisfied" name="review" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito!</p>
+        </label>
+      </div>
+
+      {/*  */}
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea
+          name="comment"
+          id="comment"
+          placeholder="Como foi a sua experiência com o produto?"
+          required
+        ></textarea>
+      </div>
     </div>
   );
 };
