9853e30456c2d993714ddac70852327cdc9680b5
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-05-20 11:06:51 +0300

    Thanks component and css

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "./Thanks.css";
+
+// Components
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
 
 const Thanks = () => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante. Em breve você receberá um cupom de 10%
+        para sua próxima compra!
+      </p>
+      <p>Para concluir sua avaliação, clique no botão de "Enviar" abaixo.</p>
+      <h3>Aqui está o resumo de sua avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
     </div>
   );
 };
